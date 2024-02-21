f0aecf5416bc08550ad8b7f5a1784006073f02b8
Dean Pierce <pierce403@gmail.com>
2024-02-21 09:11:06 -0800

    some changes for the cave, not final

--- contracts/BastetCave.sol
+++ contracts/BastetCave.sol
@@ -4,50 +4,90 @@ pragma solidity ^0.8.0;
 // BastetCave contract
 // This contract allows users to purchase BastetCrystal NFTs for USDC and sends funds to the vault
 
-// import openzeppelin contracts
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import reentrancy guard
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 // import BastetCrystal
-import "./BastetCrystal.sol";
+//import "./BastetCrystal.sol";
 
-// import BastetVault
-import "./BastetVault.sol";
+// simple interface for BastetCrystal
+interface IBastetCrystal {
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) external;
+    function orgCount() external view returns (uint256);
+    function vault() external view returns (address);
+}
+
+// CompoundFinance interface, supplyFrom(address,address,address,uint256)
+interface ICompoundFinance {
+    function supplyFrom(address from, address dst, address asst, uint256 amount) external returns (uint256);
+}   
 
 // Contract to allow users to purchase BastetCrystal NFTs with USDC
-contract BastetCave is Ownable {
+contract BastetCave is ReentrancyGuard {
     // the USDC contract address
-    address public USDCAddress;
+    address public USDCAddress = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174; 
     // the BastetCrystal contract address
-    address public BastetCrystalAddress;
-    // the BastetVault contract address
-    address public BastetVaultAddress;
-    // the owner of the contract
+    address public bastetCrystalAddress = 0xbe39Df1e59651aEF996A280B4D4212eD7b807784;
+    // the Compound Finance contract address
+    address public compoundAddress = 0xF25212E676D1F7F89Cd72fFEe66158f541246445;
+    //address public BastetVaultAddress;
 
-    // constructor to set the USDC, BastetCrystal, and BastetVault contract addresses
-    constructor(address _USDCAddress, address _BastetCrystalAddress, address _BastetVaultAddress) {
-        USDCAddress = _USDCAddress;
-        BastetCrystalAddress = _BastetCrystalAddress;
-        BastetVaultAddress = _BastetVaultAddress;
-        owner = msg.sender;
-    }
+    // mint count array
+    // the number of tokens minted *by this minting contract*
+    uint256[] public mintCount;
+    
+    // track total tributes per org id
+    uint256[] public tributeTotalByOrgId;
 
     // function to pay tribute to Bastet and obtain a crystal
-    function tribute(uint256 _id) public {
+    function tribute(uint256 _id) public nonReentrant {
 
-        // transfer USDC to the BastetVault
-        IERC20(USDCAddress).transferFrom(msg.sender, BastetVaultAddress, _amount);
-        // deposit USDC into the cUSDC contract
-        BastetVault(BastetVaultAddress).deposit(_amount);
+        // ensure id is less than number of orgs
+        require(_id < IBastetCrystal(bastetCrystalAddress).orgCount(), "invalid id");
+
+        // get the current price
+        uint256 currentPrice = getPrice(_id);
+        mintCount[_id]++;
+
+        // bump the tribute total
+        tributeTotalByOrgId[_id] += currentPrice*1000000;
+
+        // get the address of the vault from the crystal contract
+        address bastetVaultAddress = IBastetCrystal(bastetCrystalAddress).vault();
+
+        // supply from sender to vault, remember 6 decimals for USDC
+        ICompoundFinance(compoundAddress).supplyFrom(msg.sender, bastetVaultAddress, USDCAddress, currentPrice*1000000);
+
         // mint the BastetCrystal NFT
-        BastetCrystal(BastetCrystalAddress).mint(msg.sender, _id, 1, "");
+        IBastetCrystal(bastetCrystalAddress).mint(msg.sender, _id, 1, "");
     }
 
-    // function to withdraw USDC from the BastetVault
-    function withdraw(uint256 _amount) public {
-        // withdraw USDC from the cUSDC contract
-        BastetVault(BastetVaultAddress).withdraw(_amount);
-        // transfer USDC to the sender
-        IERC20(USDCAddress).transfer(msg.sender, _amount);
+    // function to tribute any amount to an org, with no NFT minted
+    function trubiteAny(uint256 _id, uint256 _amount) public nonReentrant {
+        // ensure id is less than number of orgs
+        require(_id < IBastetCrystal(bastetCrystalAddress).orgCount(), "invalid id");
+
+        // bump the tribute total
+        tributeTotalByOrgId[_id] += _amount;
+
+        // get the address of the vault from the crystal contract
+        address bastetVaultAddress = IBastetCrystal(bastetCrystalAddress).vault();
+
+        // supply from sender to vault, remember 6 decimals for USDC
+        ICompoundFinance(compoundAddress).supplyFrom(msg.sender, bastetVaultAddress, USDCAddress, _amount);
+    }
+
+    // function to get the current price by id
+    function getPrice(uint256 _id) public view returns (uint256) {
+        return mintCount[_id]+1;
+    }
+
+    // function to grow the mintCount and tributeTotal array as needed
+    function bumpArray() public {
+        // ensure the mintCount array isn't going to be bigger than the orgs array
+        require(mintCount.length < IBastetCrystal(bastetCrystalAddress).orgCount(), "this is fine");
+
+        mintCount.push(0);
+        tributeTotalByOrgId.push(0);
     }
 }
